dd80199d4eae12425fb85779354d1ce026315dc0
Juan Cisneros <jfcisnerosg@icloud.com>
2024-07-22 15:13:21 -0500

    db check

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+// Connect to Firebase
+const admin = require('firebase-admin');
+const credentials = require('./key.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials),
+});
+
+const db = admin.firestore();
+
+app.use((req, res, next) => {
+    req.db = db;
+    next();
+});
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const data = "Hello World";
+  // Access db from the request object
+  req.db.collection('data').add({data: data})
+      .then(() => res.render('index', { title: 'Express' }))
+      .catch(error => next(error)); // Handle errors
 });
 
 module.exports = router;
