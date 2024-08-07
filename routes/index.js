57c639f3f72fc708284d712cf62d543bcae5f9ef
Juan Cisneros <jfcisnerosg@icloud.com>
2024-08-07 11:25:46 -0500

    New Features: 1. Check for DB connection on index.js GET
    New Routes:
    
    Bugs Corrected:

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,17 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const data = "Hello World";
-  // Access db from the request object
-/*  req.db.collection('data').add({data: data})
-      .then(() => res.render('index', { title: 'Express' }))
-      .catch(error => next(error)); // Handle errors*/
 
-  res.status(200).send({data: data});
-});
+  //check for db connection
+    const db = req.db;
+    if (!db) {
+        return res.status(500).send({
+            error: 'Database is not connected'
+        });
+    }
 
-router.post('/', function(req, res, next) {
-  //anwser to the post request with the data obj
-  const data = {
-    name: 'juan',
-    age: 22,
-  }
-  //answer with an ok
-    res.status(200).send(data);
+    //return ok
+    res.status(200).send('Connected to the database');
 });
 
 module.exports = router;
